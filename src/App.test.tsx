ea7937d0a691aced621dffccf709508d3781395a
Jonathan Jamel Holloway <38900224+codejoncode@users.noreply.github.com>
2025-07-27 22:53:40 -0500

    got the app working error free and made adjustment to the test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders app title', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   expect(screen.getByText(/customer service helper/i)).toBeInTheDocument();
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Assistant from "./components/Assistant";
 import { LoggerPanel } from "./components/LoggerPanel";
 
 const App: React.FC = () => (
-  <div>
+  <>
     {process.env.NODE_ENV === "development" && <LoggerPanel />}
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/assistant" element={<Assistant />} />
-
-        {/* <Route path="/" element={<AgentLogin />} />
-        <Route path="/lookup" element={<MemberLookup />} />
-        <Route path="/assistant" element={
-          <BrowserRouter>
-            <CallAssistant />} /> 
-          </BrowserRouter>
-            */}
-      </Routes>
-    </Router>
-  </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/assistant" element={<Assistant />} />
+    </Routes>
+  </>
 );
 
 export default App;
